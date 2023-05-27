4d2f0898490e141ff31593c90876528896afe310
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-05-26 21:48:19 -0300

    added id on the return for getCities

--- src/controllers/citites.controllers.js
+++ src/controllers/citites.controllers.js
@@ -2,7 +2,7 @@ import { db } from "../database/database.connection.js";
 
 export async function getCities(req, res) {
     try {
-        const cities = await db.query(`SELECT cities.name AS city, states.name AS state
+        const cities = await db.query(`SELECT cities.id, cities.name AS city, states.name AS state
         FROM cities
         INNER JOIN states ON cities.state_id = states.id;`)
         res.status(200).send(cities.rows)
